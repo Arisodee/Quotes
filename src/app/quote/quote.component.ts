b682ce530a387951eec51b37df31f0253d3e844d
Arisodee <arisodee@gmail.com>
2020-11-09 02:09:00 +0300

    Add styling to the quotes layout

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,7 +12,6 @@ export class QuoteComponent implements OnInit {
     new Quote('Ariso Okanga','"We cannot be sure of having something to live for unless we are willing to die for it". ','Che Guevara', new Date (2020,11,8)),
     new Quote('Barbara Charlotte', '"There are basically two types of people. People who accomplish things, and people who claim to have accomplished things. The first group is less crowded."','Mark Twain', new Date(2020,11,5)),
   ];
-
   upVote (index){
     this.quotes[index].upVote++;
   }
