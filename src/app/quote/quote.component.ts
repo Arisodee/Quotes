824454bfb098dead9a829d8dd1ce393ddbb7fe3d
Arisodee <arisodee@gmail.com>
2020-11-09 10:17:56 +0300

    Change form layout

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -12,6 +12,10 @@ export class QuoteComponent implements OnInit {
     new Quote('Ariso Okanga','"We cannot be sure of having something to live for unless we are willing to die for it". ','Che Guevara', new Date (2020,11,8)),
     new Quote('Barbara Charlotte', '"There are basically two types of people. People who accomplish things, and people who claim to have accomplished things. The first group is less crowded."','Mark Twain', new Date(2020,11,5)),
   ];
+
+  addNewQuote(quote){
+        this.quotes.push(quote)
+  }
   upVote (index){
     this.quotes[index].upVote++;
   }
