0053dd1afbe110808ac14d77b5818ba7f03c5f2f
Arisodee <arisodee@gmail.com>
2020-11-09 12:54:37 +0300

    Add quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,6 +11,7 @@ export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
     new Quote('Ariso Okanga','"We cannot be sure of having something to live for unless we are willing to die for it". ','Che Guevara', new Date (2020,11,8)),
     new Quote('Barbara Charlotte', '"There are basically two types of people. People who accomplish things, and people who claim to have accomplished things. The first group is less crowded."','Mark Twain', new Date(2020,11,5)),
+    new Quote('Zuli Lizeti', '"If you’re always trying to be normal you will never know how amazing you can be."','Maya Angelou', new Date(2020,11,20)),
   ];
 
   addNewQuote(quote){
